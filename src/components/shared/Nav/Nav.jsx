ecff2933fec777ae08da58737cdf5a923815b86b
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2021-05-09 18:08:18 +0100

    mobile nav set up

--- src/components/shared/Nav/Nav.jsx
+++ src/components/shared/Nav/Nav.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import useWindowSize from '../../../hooks/useWindowSize';
+import styles from './Nav.module.scss';
+import ResponsiveImage from '../ResponsiveImage/ResponsiveImage';
+
+const NavMobile = () => (
+  <nav className={styles.nav}>
+    <div className={styles.contents}>
+      <button className={`${styles.nav_button} ${styles.menu}`} type="button">
+        &nbsp;
+      </button>
+      <div className={styles.logo}>
+        <ResponsiveImage
+          src="/assets/shared/desktop/logo.svg"
+          alt="Audiophile logo"
+        />
+      </div>
+      <button className={`${styles.nav_button} ${styles.cart}`} type="button">
+        &nbsp;
+      </button>
+    </div>
+  </nav>
+);
 
 function Nav() {
   const windowSize = useWindowSize();
-  return windowSize > 500 ? <h1>Greater</h1> : <h2>Lower</h2>;
+  return windowSize < 500 ? <NavMobile /> : <h2>Desktop</h2>;
 }
 
 export default Nav;
