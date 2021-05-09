af2a23787aea68019cb9cd4cd19b9cc99ac9e02a
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2021-05-09 12:37:15 +0100

    initial mixins and global styles, as well as assets for site

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import '../src/style/styles.css';
+import Head from 'next/head';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
