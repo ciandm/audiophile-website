e868936875f0b3e824e9dd47023cf1812bb29a24
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2021-05-16 21:00:31 +0100

    added hero section component

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -5,16 +5,18 @@ import styles from './HeroSection.module.scss';
 function HeroSection() {
   return (
     <section className={styles.section}>
-      <div className={styles.content}>
-        <span className={styles.tagline}>New product</span>
-        <h1 className={styles.title}>XX99 Mark II Headphone</h1>
-        <p className={styles.description}>
-          Experience natural, lifelike audio and exceptional build quality made
-          for the passionate music enthusiast.
-        </p>
-        <Button type="link" href="/products/xx99-mark-two-headphones">
-          See product
-        </Button>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <span className={styles.tagline}>New product</span>
+          <h1 className={styles.title}>XX99 Mark II Headphone</h1>
+          <p className={styles.description}>
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+          <Button type="link" href="/products/xx99-mark-two-headphones">
+            See product
+          </Button>
+        </div>
       </div>
     </section>
   );
